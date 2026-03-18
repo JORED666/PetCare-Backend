3395cc4167331b241f82d931f6faddaed97a2746
JORED666 <243842@ids.upchiapas.edu.mx>
2026-03-18 00:30:31 -0600

    fix: routes pet service

--- services/pet-service/src/infrastructure/http/routes/routes.ts
+++ services/pet-service/src/infrastructure/http/routes/routes.ts
@@ -9,20 +9,20 @@ import { GetPacientesDetalleController } from '../controllers/pets/GetPacientesD
 
 const router = Router();
 
-const getAllPetsController = new GetAllPetsController();
-const getPetByIdController = new GetPetByIdController();
-const getPetsByUserIdController = new GetPetsByUserIdController();
-const createPetController = new CreatePetController();
-const updatePetController = new UpdatePetController();
-const deletePetController = new DeletePetController();
-const getPacientesDetalleController = new GetPacientesDetalleController();
+const getAllPetsController            = new GetAllPetsController();
+const getPetByIdController            = new GetPetByIdController();
+const getPetsByUserIdController       = new GetPetsByUserIdController();
+const createPetController             = new CreatePetController();
+const updatePetController             = new UpdatePetController();
+const deletePetController             = new DeletePetController();
+const getPacientesDetalleController   = new GetPacientesDetalleController();
 
-router.get('/pets', getAllPetsController.handle.bind(getAllPetsController));
-router.get('/pets/:id', getPetByIdController.handle.bind(getPetByIdController));
-router.get('/pets/user/:userId', getPetsByUserIdController.handle.bind(getPetsByUserIdController));
-router.post('/pets', createPetController.handle.bind(createPetController));
-router.put('/pets/:id', updatePetController.handle.bind(updatePetController));
-router.delete('/pets/:id', deletePetController.handle.bind(deletePetController));
-router.get('/pets/detalle', getPacientesDetalleController.handle.bind(getPacientesDetalleController));
+router.get('/pets/detalle',          getPacientesDetalleController.handle.bind(getPacientesDetalleController));
+router.get('/pets/user/:userId',     getPetsByUserIdController.handle.bind(getPetsByUserIdController));
+router.get('/pets',                  getAllPetsController.handle.bind(getAllPetsController));
+router.get('/pets/:id',              getPetByIdController.handle.bind(getPetByIdController));
+router.post('/pets',                 createPetController.handle.bind(createPetController));
+router.put('/pets/:id',              updatePetController.handle.bind(updatePetController));
+router.delete('/pets/:id',           deletePetController.handle.bind(deletePetController));
 
 export default router;
