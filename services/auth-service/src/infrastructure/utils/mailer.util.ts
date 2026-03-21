89bbd476b0d26893f85e25b071503d163350d0c6
JORED666 <243842@ids.upchiapas.edu.mx>
2026-03-21 13:06:46 -0600

    fix: for email auth service mailer

--- services/auth-service/src/infrastructure/utils/mailer.util.ts
+++ services/auth-service/src/infrastructure/utils/mailer.util.ts
@@ -2,13 +2,9 @@ import nodemailer, { TransportOptions } from 'nodemailer';
 
 function getTransporter() {
   return nodemailer.createTransport({
-    host: '74.125.133.108',
-    port: 587,
-    secure: false,
-    tls: {
-      rejectUnauthorized: false,
-      servername: 'smtp.gmail.com'
-    },
+    host: 'smtp.gmail.com',
+    port: 465,
+    secure: true,
     auth: {
       user: process.env.GMAIL_USER,
       pass: process.env.GMAIL_APP_PASSWORD,
