7c1eee0c049dd6101caa1c265ac2890549a0fee0
JORED666 <243842@ids.upchiapas.edu.mx>
2026-03-21 20:33:47 -0600

    fix: pet service routes

--- services/pet-service/src/infrastructure/http/routes/routes.ts
+++ services/pet-service/src/infrastructure/http/routes/routes.ts
@@ -20,13 +20,13 @@ const updatePetController           = new UpdatePetController();
 const deletePetController           = new DeletePetController();
 const getPacientesDetalleController = new GetPacientesDetalleController();
 
-// Rutas específicas primero
+
 router.get('/pets/detalle',           getPacientesDetalleController.handle.bind(getPacientesDetalleController));
 router.get('/pets/user/:userId',      getPetsByUserIdController.handle.bind(getPetsByUserIdController));
 router.patch('/pets/:id/reactivar',   reactivarPetController.handle.bind(reactivarPetController));
 router.delete('/pets/:id/permanente', deletePermanentePetController.handle.bind(deletePermanentePetController));
 
-// Rutas genéricas después
+
 router.get('/pets',        getAllPetsController.handle.bind(getAllPetsController));
 router.get('/pets/:id',    getPetByIdController.handle.bind(getPetByIdController));
 router.post('/pets',       createPetController.handle.bind(createPetController));
